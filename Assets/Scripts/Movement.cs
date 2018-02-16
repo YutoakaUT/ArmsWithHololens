224330e60eebd38314379692d5da4b4b83e29770
DESKTOP-IBB5Q3O\hasee <951367242@qq.com>
2018-02-16 15:43:42 +0900

    改良

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	public float moveSpeed;
-	public float turnSpeed;
 	private Rigidbody rb;
 	private float movementInputValue;
 	private float turnInputValue;
@@ -18,27 +17,34 @@ public class Movement : MonoBehaviour {
 		Move();
 		Turn();
 		Jump();
+		Assault();
 	}
 
 	// 前後移動のメソッド
 	void Move(){
 		movementInputValue = Input.GetAxis("Vertical");
-		Vector3 movement = transform.forward * movementInputValue * moveSpeed * Time.deltaTime;
+		Vector3 movement = transform.forward * movementInputValue * Time.deltaTime;
 		movement.y*=0;
-		rb.MovePosition(rb.position + movement.normalized/6);
+		rb.MovePosition(rb.position + movement.normalized * moveSpeed / 10);
 	}
 
 	// 左右移動のメソッド
 	void Turn(){
 		movementInputValue = Input.GetAxis("Horizontal");
-		Vector3 movement = transform.right * movementInputValue * moveSpeed * Time.deltaTime;
+		Vector3 movement = transform.right * movementInputValue * Time.deltaTime;
 		movement.y*=0;
-		rb.MovePosition(rb.position + movement.normalized/6);
+		rb.MovePosition(rb.position + movement.normalized * moveSpeed / 10);
 	}
 	
 	void Jump()
 	{
 		if (Input.GetKeyDown(KeyCode.Space))
 			rb.velocity+=new Vector3(0,10,0);
+	}
+	
+	void Assault()
+	{
+		if (Input.GetKey(KeyCode.Q)&&Input.GetKey(KeyCode.E))
+			rb.velocity = transform.forward.normalized*100;
 	}
 }
