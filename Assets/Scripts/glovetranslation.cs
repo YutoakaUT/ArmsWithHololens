bed2765cbb5b1d02d0b69f6800ad6e6e7b70b88e
igarashi <xarther98@BP.local>
2018-02-20 11:58:23 +0900

    ver1.23 拳のモデルを投入しました

---
+++ Assets/Scripts/glovetranslation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glovetranslation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = 
+			new Vector3 ( transform.localScale.x, transform.localScale.y, transform.localScale.z * -1 );
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
