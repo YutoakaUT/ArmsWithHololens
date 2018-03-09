0da4dd51b21aa59af9520b7845aa76fdaca0e653
rm2c <tomi.ageha@gmail.com>
2018-03-09 16:21:46 +0900

    バネのブレを修正しました

--- Assets/Scripts/Target_making.cs
+++ Assets/Scripts/Target_making.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+/*
+注意：标靶的形状最好严格为圆形
+的の形は円の方がいい
+*/
 public class Target_making : MonoBehaviour
 {
     public GameObject target_normal;
 	public GameObject target_hard;
 	public GameObject target_small;
+	public Transform leftwall,rightwall/*yz*/,frontwall,backwall/*xy*/;
 	public bool Debug=false;
 	public static List<GameObject> targetz = new List<GameObject>();
 	int countt=0,wall;
 	public static int tar=0;//再生成の信号
 	int nor_r=1,har_r=1,sma_r=1;//各種類の的が占めている比例
+	float[,] para=new float[4,5];//para[0]=={xmin,xmax,ymin,ymax,z}或いは{x,ymin,ymax,zmin,zmax}
 
 
     float temp_x, temp_y,temp_z;  //座標のバッファ
@@ -19,6 +25,29 @@ public class Target_making : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+		para[0,0]=frontwall.position.x-frontwall.lossyScale.x/2;
+		para[0,1]=frontwall.position.x+frontwall.lossyScale.x/2;
+		para[0,2]=frontwall.position.y-frontwall.lossyScale.y/2;
+		para[0,3]=frontwall.position.y+frontwall.lossyScale.y/2;
+		para[0,4]=frontwall.position.z-frontwall.lossyScale.z/2;
+		
+		para[1,0]=backwall.position.x-backwall.lossyScale.x/2;
+		para[1,1]=backwall.position.x+backwall.lossyScale.x/2;
+		para[1,2]=backwall.position.y-backwall.lossyScale.y/2;
+		para[1,3]=backwall.position.y+backwall.lossyScale.y/2;
+		para[1,4]=backwall.position.z+backwall.lossyScale.z/2;
+		
+		para[2,0]=leftwall.position.x+leftwall.lossyScale.x/2;
+		para[2,1]=leftwall.position.y-leftwall.lossyScale.y/2;
+		para[2,2]=leftwall.position.y+leftwall.lossyScale.y/2;
+		para[2,3]=leftwall.position.z-leftwall.lossyScale.z/2;
+		para[2,4]=leftwall.position.z+leftwall.lossyScale.z/2;
+		
+		para[3,0]=rightwall.position.x-rightwall.lossyScale.x/2;
+		para[3,1]=rightwall.position.y-rightwall.lossyScale.y/2;
+		para[3,2]=rightwall.position.y+rightwall.lossyScale.y/2;
+		para[3,3]=rightwall.position.z-rightwall.lossyScale.z/2;
+		para[3,4]=rightwall.position.z+rightwall.lossyScale.z/2;
 		createtarget(200);
     }
 
@@ -60,9 +89,12 @@ public class Target_making : MonoBehaviour
 		createhard(har);
 		createsmall(sma);
 	}
+	/*前後左右*/
 	void createnormal(int number)
 	{
 		bool create=true;
+		float radii=target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2;
+		float thickness=target_normal.GetComponentInChildren<Renderer>().bounds.size.z;
 		while (create&&number-->0)
 		{
 	A:		if(countt>=100)
@@ -77,27 +109,27 @@ public class Target_making : MonoBehaviour
 				wall=3;
 			if(wall==1)
 			{
-				temp_x = Random.Range(-24.5f+target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_y = Random.Range(target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = 24.5f-target_normal.GetComponentInChildren<Renderer>().bounds.size.z;
+				temp_x = Random.Range(para[0,0]+radii,para[0,1]-radii);
+				temp_y = Random.Range(para[0,2]+radii,para[0,3]-radii);
+				temp_z = para[0,4]-thickness;
 			}
 			else if(wall==2)
 			{
-				temp_x = Random.Range(-24.5f+target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_y = Random.Range(target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = target_normal.GetComponentInChildren<Renderer>().bounds.size.z-24.5f;
+				temp_x = Random.Range(para[1,0]+radii,para[1,1]-radii);
+				temp_y = Random.Range(para[1,2]+radii,para[1,3]-radii);
+				temp_z = thickness+para[1,4];
 			}
 			else if(wall==3)
 			{
-				temp_x = target_normal.GetComponentInChildren<Renderer>().bounds.size.z-24.5f;
-				temp_y = Random.Range(target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = Random.Range(-24.5f+target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2);
+				temp_x = thickness+para[2,0];
+				temp_y = Random.Range(para[2,1]+radii,para[2,2]-radii);
+				temp_z = Random.Range(para[2,3]+radii,para[2,4]-radii);
 			}
 			else if(wall==4)
 			{
-				temp_x = 24.5f-target_normal.GetComponentInChildren<Renderer>().bounds.size.z;
-				temp_y = Random.Range(target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = Random.Range(-24.5f+target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2);
+				temp_x = para[3,0]-thickness;
+				temp_y = Random.Range(para[3,1]+radii,para[3,2]-radii);
+				temp_z = Random.Range(para[3,3]+radii,para[3,4]-radii);
 			}
 			if(Debug==true)
 				temp_y=2;
@@ -105,8 +137,8 @@ public class Target_making : MonoBehaviour
 			foreach (GameObject gameo in targetz)
 			{
 				if((gameo.transform.position-new Vector3(temp_x,temp_y,temp_z)).sqrMagnitude<
-				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2)
-				*(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+target_normal.GetComponentInChildren<Renderer>().bounds.size.x/2))
+				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+radii)*
+				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+radii))
 				{
 					countt++;
 					goto A;    //近すぎる場合は破棄し、再生成する
@@ -129,6 +161,8 @@ public class Target_making : MonoBehaviour
 	void createhard(int number)
 	{
 		bool create=true;
+		float radii=target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2;
+		float thickness=target_hard.GetComponentInChildren<Renderer>().bounds.size.z;
 		while (create&&number-->0)
 		{
 A:		if(countt>=100)
@@ -143,27 +177,27 @@ A:		if(countt>=100)
 				wall=3;
 			if(wall==1)
 			{
-				temp_x = Random.Range(-24.5f+target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_y = Random.Range(target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = 24.5f-target_hard.GetComponentInChildren<Renderer>().bounds.size.z;
+				temp_x = Random.Range(para[0,0]+radii,para[0,1]-radii);
+				temp_y = Random.Range(para[0,2]+radii,para[0,3]-radii);
+				temp_z = para[0,4]-thickness;
 			}
 			else if(wall==2)
 			{
-				temp_x = Random.Range(-24.5f+target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_y = Random.Range(target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = target_hard.GetComponentInChildren<Renderer>().bounds.size.z-24.5f;
+				temp_x = Random.Range(para[1,0]+radii,para[1,1]-radii);
+				temp_y = Random.Range(para[1,2]+radii,para[1,3]-radii);
+				temp_z = thickness+para[1,4];
 			}
 			else if(wall==3)
 			{
-				temp_x = target_hard.GetComponentInChildren<Renderer>().bounds.size.z-24.5f;
-				temp_y = Random.Range(target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = Random.Range(-24.5f+target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2);
+				temp_x = thickness+para[2,0];
+				temp_y = Random.Range(para[2,1]+radii,para[2,2]-radii);
+				temp_z = Random.Range(para[2,3]+radii,para[2,4]-radii);
 			}
 			else if(wall==4)
 			{
-				temp_x = 24.5f-target_hard.GetComponentInChildren<Renderer>().bounds.size.z;
-				temp_y = Random.Range(target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = Random.Range(-24.5f+target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2);
+				temp_x = para[3,0]-thickness;
+				temp_y = Random.Range(para[3,1]+radii,para[3,2]-radii);
+				temp_z = Random.Range(para[3,3]+radii,para[3,4]-radii);
 			}
 			if(Debug==true)
 				temp_y=2;
@@ -171,8 +205,8 @@ A:		if(countt>=100)
 			foreach (GameObject gameo in targetz)
 			{
 				if((gameo.transform.position-new Vector3(temp_x,temp_y,temp_z)).sqrMagnitude<
-				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2)*
-				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+target_hard.GetComponentInChildren<Renderer>().bounds.size.x/2))
+				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+radii)*
+				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+radii))
 				{
 					countt++;
 					goto A;    //近すぎる場合は破棄し、再生成する
@@ -195,6 +229,8 @@ A:		if(countt>=100)
 	void createsmall(int number)
 	{
 		bool create=true;
+		float radii=target_small.GetComponentInChildren<Renderer>().bounds.size.x/2;
+		float thickness=target_small.GetComponentInChildren<Renderer>().bounds.size.z;
 		while (create&&number-->0)
 		{
 	A:		if(countt>=100)
@@ -209,27 +245,27 @@ A:		if(countt>=100)
 				wall=3;
 			if(wall==1)
 			{
-				temp_x = Random.Range(-24.5f+target_small.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_small.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_y = Random.Range(target_small.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_small.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = 24.5f-target_small.GetComponentInChildren<Renderer>().bounds.size.z;
+				temp_x = Random.Range(para[0,0]+radii,para[0,1]-radii);
+				temp_y = Random.Range(para[0,2]+radii,para[0,3]-radii);
+				temp_z = para[0,4]-thickness;
 			}
 			else if(wall==2)
 			{
-				temp_x = Random.Range(-24.5f+target_small.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_small.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_y = Random.Range(target_small.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_small.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = target_small.GetComponentInChildren<Renderer>().bounds.size.z-24.5f;
+				temp_x = Random.Range(para[1,0]+radii,para[1,1]-radii);
+				temp_y = Random.Range(para[1,2]+radii,para[1,3]-radii);
+				temp_z = thickness+para[1,4];
 			}
 			else if(wall==3)
 			{
-				temp_x = target_small.GetComponentInChildren<Renderer>().bounds.size.z-24.5f;
-				temp_y = Random.Range(target_small.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_small.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = Random.Range(-24.5f+target_small.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_small.GetComponentInChildren<Renderer>().bounds.size.x/2);
+				temp_x = thickness+para[2,0];
+				temp_y = Random.Range(para[2,1]+radii,para[2,2]-radii);
+				temp_z = Random.Range(para[2,3]+radii,para[2,4]-radii);
 			}
 			else if(wall==4)
 			{
-				temp_x = 24.5f-target_small.GetComponentInChildren<Renderer>().bounds.size.z;
-				temp_y = Random.Range(target_small.GetComponentInChildren<Renderer>().bounds.size.x/2,50-target_small.GetComponentInChildren<Renderer>().bounds.size.x/2);
-				temp_z = Random.Range(-24.5f+target_small.GetComponentInChildren<Renderer>().bounds.size.x/2,24.5f-target_small.GetComponentInChildren<Renderer>().bounds.size.x/2);
+				temp_x = para[3,0]-thickness;
+				temp_y = Random.Range(para[3,1]+radii,para[3,2]-radii);
+				temp_z = Random.Range(para[3,3]+radii,para[3,4]-radii);
 			}
 			if(Debug==true)
 				temp_y=2;
@@ -238,8 +274,8 @@ A:		if(countt>=100)
 			foreach (GameObject gameo in targetz)
 			{
 				if((gameo.transform.position-new Vector3(temp_x,temp_y,temp_z)).sqrMagnitude<
-				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+target_small.GetComponentInChildren<Renderer>().bounds.size.x/2)
-				*(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+target_small.GetComponentInChildren<Renderer>().bounds.size.x/2))
+				(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+radii)
+				*(gameo.GetComponentInChildren<Renderer>().bounds.size.y/2+radii))
 				{
 					countt++;
 					goto A;    //近すぎる場合は破棄し、再生成する
---
+++ Assets/modetrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class modetrigger : MonoBehaviour 
+{
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			GetComponent<Animator> ().SetTrigger ("MouseOn");
+		}
+	}
+}
